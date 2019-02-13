de0754614342e699995310593d6450ea08c84e7a
EasonWuTPE <tvjs168@gmail.com>
2019-02-13 11:56:23 +0800

    Commits

--- PythonForFinance/np.py
+++ PythonForFinance/np.py
@@ -2,6 +2,7 @@
 
 import numpy as np 
 
+'''     Basice Numpy '''
 ''' 
  np.array only exist one data type.  
 ''' 
@@ -41,6 +42,13 @@ print( arr4 )
 arr5 = np.array([[1,2.3],[1,3]]) # np.array only exist one data type.  
 print( arr5 ) 
 
+# the order arg is the order in which to store elements in memory 
+# order ='C' for c-like, row-wise, 'F' for fortran-like, column-wise  
+arr6 = np.ones((2,3,4),order='C') 
+print('row-wise:\n',arr6) 
+arr7 = np.ones((2,3,4),order='F') 
+print('column-wise:\n',arr7) 
+
 #############################################################################
 
 
@@ -64,6 +72,37 @@ starttime = time.time()
 MATRIX_Numpy = np.random.standard_normal( (SIZE,SIZE) ) 
 endtime = time.time() 
 print( "Duration by Numpy: ", endtime-starttime ) 
+
+############################################################################
+
+'''     Structured Arrays: different dtype per columns    '''
+# Defined the columns with differents data type by np.dtype(). 
+defined_dt = np.dtype( [("Name","S10"),("Age","i4"),
+                        ("Height","f"),("Children/Pets","i4",2)] ) 
+s = np.array( [ ("EasonWu",23,1.74,(1,0)),
+                ("Leticia",23,1.68,(1,0)) ], dtype=defined_dt ) 
+print(s) 
+print(s["Name"]) 
+print(s[0]["Name"]) 
+print(s["Name"][0]) 
+print(s["Height"].mean()) 
+
+'''              Basice Vectorization                    ''' 
+r =np.random.standard_normal((4,3)) 
+s =np.random.standard_normal((4,3)) 
+
+print(r+s) 
+print(2*r+3)# Broadcast  
+
+n = np.random.standard_normal(3) 
+print( r+n ) # row-wise addition  
+#print( r.transpose()+n ) # ERRORs   
+
+def f(x): 
+    return 2*x+1 
+print( f(np.ones((3,4))) ) # Passing a numpy matrix to function calcultaion  
+
+
 
 
 
