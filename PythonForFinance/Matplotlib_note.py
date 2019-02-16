4e7cbe7c8c44dbc86a6ab65dc89bcf7792205781
EasonWuTPE <tvjs168@gmail.com>
2019-02-16 15:34:56 +0800

    Commits

--- PythonForFinance/Matplotlib_note.py
+++ PythonForFinance/Matplotlib_note.py
@@ -94,3 +94,60 @@ plt.xlim(-1,20)
 plt.ylim(np.min(y.cumsum())-1, np.max(y.cumsum())+1) 
 plt.axis('tight') 
 plt.show() 
+
+# Scatter plot 
+y = np.random.standard_normal((1000,2)) 
+plt.figure(figsize=(7,5)) 
+plt.title("FIgure VII") 
+plt.scatter(y[:,0],y[:,1],color='r', marker='o') 
+plt.grid(True) 
+plt.xlabel("1st") 
+plt.ylabel("2nd") 
+plt.show() 
+
+
+# Scatter plot with color bar 
+c = np.random.randint(0,10, len(y)) 
+plt.figure(figsize=(7,5)) 
+plt.title("FIgure VII with color bar") 
+plt.scatter(y[:,0],y[:,1],c=c,marker='o') 
+plt.colorbar() # set color bar 
+plt.grid(True) 
+plt.xlabel("1st") 
+plt.ylabel("2nd") 
+plt.show() 
+
+
+# Histogram 
+plt.figure(figsize=(7,4)) 
+plt.hist( y, label=['1st','2nd'], bins=50 ) 
+plt.grid(True) 
+plt.legend(loc='best') 
+plt.ylabel('Frequency') 
+plt.xlabel('value') 
+plt.title('Figure VIII') 
+plt.show() 
+
+
+# Histogram with stack  
+plt.figure(figsize=(7,4)) 
+plt.hist( y, label=['1st','2nd'], bins=50, stacked=True, color=['r','g'] ) 
+plt.grid(True) 
+plt.legend(loc='best') 
+plt.ylabel('Frequency') 
+plt.xlabel('value') 
+plt.title('Figure VIII with stack') 
+plt.show() 
+
+
+# Box Plot 
+fig, ax = plt.subplots(figsize=(7,4)) 
+plt.boxplot(y) 
+plt.grid(True) 
+plt.setp(ax,xticklabels=['1st','2nd']) # setting properties of plotting. 
+plt.xlabel('data set') 
+plt.ylabel('value') 
+plt.title('Figure IX') 
+plt.show() 
+
+
