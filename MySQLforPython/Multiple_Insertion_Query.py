3637783c9493673d32205753d3e935c6d69ce19d
EasonWuTPE <tvjs168@gmail.com>
2019-03-04 21:49:03 +0800

    commit modified

--- MySQLforPython/Multiple_Insertion_Query.py
+++ MySQLforPython/Multiple_Insertion_Query.py
@@ -18,17 +18,29 @@ import MySQLdb as mysql
 
 ''' 
 
-mydb = mysql.connect( host='localhost',user='root',passwd='Tv0912548857',db='students' ) 
+mydb = mysql.connect( host='localhost',user='root',passwd='***',db='students' ) 
 cur_ = mydb.cursor() 
 
 data = [ ["apple",1254364,176],["cat",2317453,192] ] 
 #data = [ ("apple",1254364,176),("cat",2317453,192) ] 
-#statements1 = "insert into studentstable( name, school_id,height) values(%s,%s,%s);" 
-#cur_.executemany( statements1, data ) 
-#cur_.close() 
+statements1 = "insert into studentstable( name, school_id,height) values(%s,%s,%s);" 
+cur_.executemany( statements1, data ) 
+cur_.close() 
 
-statements2 = "select * from studentstable where name=(%s,%s);" 
-command = cur_.executemany( statements2, ('xyz','eee') ) 
+#statements2 = "select * from studentstable where name=(%s,%s);" 
+#command = cur_.executemany( statements2, ('xyz','eee') ) 
+
+''' 
+    executemany() can boost program. However, it has its limit. It will lost connection to MySQL.
+    This is because MySQL's maximum default pocket size is 1 MB and the maximum data that the server will recieve per pocket is 1 MB. 
+    
+    Two ways to solve this problem: 
+
+    1. Decreasing the amount of data that you are passing to MySQL in each packet. 
+    2. Increase MySQL's torelance for data by command-line if you are administrator. 
+        See more on p.173 in MySQL for Python 
+''' 
+
 
 mydb.commit() 
 mydb.close() 
